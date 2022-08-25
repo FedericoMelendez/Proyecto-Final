39a1f7571884a7758a1353376e764c00dba1e484
Federico Lopez <fede.lopez.tejera@gmail.com>
2022-08-25 15:17:25 -0300

    start

--- NucleoProyecto/NucleoProyecto/urls.py
+++ NucleoProyecto/NucleoProyecto/urls.py
@@ -1,7 +1,7 @@
 from itertools import product
 from operator import index
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from NucleoProyecto.views import saludo,index
 
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path ('saludo/', saludo, name='saludo'), #Prueba del servidor con la funcion saludo
     path('products/',include ('products.urls')),
-    path ('', index, name='index')
+    path ('', index, name='index'),
+    path('users/', include ('users.urls')),
 ]
