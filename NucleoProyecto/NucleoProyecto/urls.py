3537d545f93a00e13e1ecc48604239ace596aef0
FedericoMelendez <federiconahuelmelendez@gmail.com>
2022-08-25 00:43:56 -0300

    creacion navbar

--- NucleoProyecto/NucleoProyecto/urls.py
+++ NucleoProyecto/NucleoProyecto/urls.py
@@ -2,11 +2,12 @@ from itertools import product
 from operator import index
 from django.contrib import admin
 from django.urls import path,include
-from NucleoProyecto.views import saludo
+from NucleoProyecto.views import saludo,index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('saludo/', saludo, name='saludo'), #Prueba del servidor con la funcion saludo
-    path('products/',include ('products.urls'))
+    path('products/',include ('products.urls')),
+    path ('', index, name='index')
 ]
--- NucleoProyecto/NucleoProyecto/views.py
+++ NucleoProyecto/NucleoProyecto/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 
 def saludo (request):
     return HttpResponse('Bienvenidos a todos ') #Prueba de servidor con la funcion saludo.
+
+def index(request):
+    return render (request, 'index.html')
+
+    
