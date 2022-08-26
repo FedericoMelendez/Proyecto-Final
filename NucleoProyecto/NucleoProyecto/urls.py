c952f45f41d1323a1ea2d4110bcfcddf1be41905
FedericoMelendez <federiconahuelmelendez@gmail.com>
2022-08-26 00:36:14 -0300

    prueba

--- NucleoProyecto/NucleoProyecto/urls.py
+++ NucleoProyecto/NucleoProyecto/urls.py
@@ -2,6 +2,7 @@ from itertools import product
 from operator import index
 from django.contrib import admin
 from django.urls import path, include
+
 from NucleoProyecto.views import saludo,index
 
 
